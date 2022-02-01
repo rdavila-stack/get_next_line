d09621d56e15c4107228d591f3805a922d0902f2
Ramon Davila <78738387+rdavila-stack@users.noreply.github.com>
2022-02-01 12:35:30 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -3,33 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdavila <rdavila@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rdavila <rdavila@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 13:28:50 by rdavila           #+#    #+#             */
-/*   Updated: 2022/01/26 13:18:01 by rdavila          ###   ########.fr       */
+/*   Updated: 2022/02/01 11:16:35 by rdavila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	*ft_calloc(size_t count, size_t size)
+size_t	ft_strlen(const char *str)
 {
-	char				*dest;
-	unsigned int		total;
-	unsigned int		i;
+	int	output;
 
-	total = count * size;
-	dest = malloc(total);
-	if (!dest)
-		return (0);
-	i = 0;
-	while (total--)
+	output = 0;
+	while (*str != '\0')
 	{
-		dest[i] = 0;
-		i++;
+		output++;
+		str++;
 	}
-	return ((void *)dest);
+	return (output);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -68,31 +62,86 @@ char	*ft_strchr( const char *s, int c)
 	return (0);
 }
 
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	char	*ptr;
+	int		len;
+
+	len = 0;
+	while (s1[len])
+		len ++;
+	str = malloc (len + 1);
+	if (!str)
+		return (0);
+	ptr = str;
+	while (*s1)
+		*ptr++ = *s1++;
+	*ptr = '\0';
+	return (str);
+}
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	while (i < len)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+
+char	*ft_get_line(char	**line)
+{
+	int		i;
+	char	*string;
+	char	*newstring;
+	i = 0;
+	
+	while(*line[i] != '\n' && *line[i] != 0)
+	{
+		i++;
+	}
+	string = ft_substr(*line, 0, i);
+	newstring = ft_substr(*line, i + 1, ft_strlen(*line) - i - 1);
+	free(*line);
+	*line = newstring;
+	return (string);
+}
 
 char	*get_next_line(int fd)
 {
 	char		*buf;
+	char		*output;
 	static char	*line;
 	ssize_t		readlen;
 
-	buf = ft_calloc((BUFFER_SIZE + 1) * sizeof (*buf));
+	buf = malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
-	
 	readlen = read(fd, buf, BUFFER_SIZE);
 	while (readlen > 0)
 	{
-		
 		buf [readlen] = 0;
 		line = ft_strjoin (line, buf);
 		if (ft_strchr(line, '\n'))
 			break ;
 		readlen = read(fd, buf, BUFFER_SIZE);
-		//return (line); 
 	}
+	output = ft_get_line(&line); //output = ft_get_line
 	free(buf);
-	return(&line);
-	
+	return(output);
 }
 
 int	main()
@@ -101,11 +150,10 @@ int	main()
 	fd = open("main.txt", O_RDONLY);
 	char *junkie;
 	junkie = get_next_line(fd);
-	
 	while (junkie != NULL)
 	{
-		junkie = get_next_line(fd);
 		printf("%s \n", junkie);
+		junkie = get_next_line(fd);
 	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdavila <rdavila@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rdavila <rdavila@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 13:29:27 by rdavila           #+#    #+#             */
-/*   Updated: 2022/01/24 14:04:44 by rdavila          ###   ########.fr       */
+/*   Updated: 2022/02/01 11:10:17 by rdavila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# define BUFFER_SIZE 20
 
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c)
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-char	*ft_strchr(const char *s, int c)
-size_t	ft_strlen(const char *s)
-char	*ft_strjoin(const char *s1, const char *s2)
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
 
 
 #endif
